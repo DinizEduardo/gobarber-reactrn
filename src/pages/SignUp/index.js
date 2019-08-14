0650c60722e39b638c0405297158aa17a289c9d6
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-08-14 20:50:46 -0300

    Configuração das rotas de login/cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 
 // import { Container } from './styles';
-import Routes from './routes';
 
-export default function App() {
-  return <Routes />;
+export default function SignUp() {
+  return <View />;
 }
