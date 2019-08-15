9819f7647795dc92d8f4fad73c10b97a730449c4
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-08-14 21:49:22 -0300

    Configurações do reactotron

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StatusBar , View } from 'react-native';
-
-
+import { StatusBar, View } from 'react-native';
+import './config/ReactotronConfig';
 // import { Container } from './styles';
 import Routes from './routes';
 
