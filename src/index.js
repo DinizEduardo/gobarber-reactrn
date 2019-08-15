692088b72d330d99d0bcc1747cc8fe33fe08f348
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-08-15 18:19:40 -0300

    Configuração do redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { StatusBar, View } from 'react-native';
 import './config/ReactotronConfig';
 // import { Container } from './styles';
 import Routes from './routes';
 
+import { store, persistor } from './store';
+
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }
