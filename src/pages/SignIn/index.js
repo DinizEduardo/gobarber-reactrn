4cc742088a4c61ad736c88b5493b9dbd1da0c32b
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-08-14 21:30:04 -0300

    Estilização da pagina de signin

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignText,
+} from './styles';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
-      <Input
-        icon="call"
-        placeholder="Digite seu nome"
-        style={{ marginTop: 30 }}
-      />
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+          <SignLink onPress={() => {}}>
+            <SignText>Criar conta gratuita</SignText>
+          </SignLink>
+        </Form>
+      </Container>
     </Background>
   );
 }
